59b4424f61a217f399de3992af06ef14bd49c92a
parthilasariya <54209676+parthilasariya@users.noreply.github.com>
2021-11-29 18:18:11 -0500

    Loginapp

--- Login Form/ViewController.swift
+++ Login Form/ViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func userTextField(_ sender: UITextField) {
+    }
+    @IBAction func passwordTextField(_ sender: UITextField) {
+    }
+    @IBAction func signinButton(_ sender: Any) {
+    }
+    
+    var radius = 22
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      
+
     }
 
 
